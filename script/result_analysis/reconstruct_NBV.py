9089cb5ee1b6a052899200b7fabddaca8ff4c073
yiming <wangyimingkaren@gmail.com>
2020-09-22 18:59:17 +0200

    fixed visualisation bugs in recontruct_NBV.py

--- script/result_analysis/reconstruct_NBV.py
+++ script/result_analysis/reconstruct_NBV.py
@@ -13,7 +13,6 @@ Step3: rerun the script, you can set VIS_ALL = True to get visualisation at each
 import utils.filetool as ft
 import utils.io as io
 import utils.plot as local_plt
-import utils.transformation as tf
 import utils.improc as improc
 import os
 import numpy as np
@@ -32,7 +31,7 @@ vox_num_option = "o3d"
 
 
 VIS_FINAL = True
-VIS_ALL = False
+VIS_ALL = True
 
 NBV_strategy = ["CNNCombInfoGain","KnownDepthTwo", "GainLargeTwo","Random", "CNNmemoTwoStep5D", "CNNmemoTwoStep2D","CNNdepth","CNNMLPTwoStep5D", "CNNMLPTwoStep2D"]
 
@@ -52,10 +51,11 @@ temp_folder = config["temp_folder"] # all info are only saved at run time and wi
 selected_room_names = all_room_names
 radius_list = [0.1] # unit is in meter
 
-label_list = ["up","down","left","right"]
+label_list = ["up", "down", "left", "right"]
 intrinsic_file = os.path.join(proj_path, param_folder, intrinsic_file_name)
 intrinsic = o3d.read_pinhole_camera_intrinsic(intrinsic_file)
 print(intrinsic)
+
 ## empiracally tried,  but the real kinect intrinsics works better
 K = np.eye(3)
 K[0,0] = 585 #595  #590 #573 #690
@@ -75,8 +75,7 @@ start_key_list = ["250"] # the start indices being experimented are: [45, 168, 2
 # function to save the visualiser's view point
 # (it is not related to the camera pose of the dataset, only for visualisation)
 # the new view point file will be saved into filename.
-def save_view_point(pcd, filename):
-    vis = o3d.Visualizer()
+def save_view_point(vis, pcd, filename):
     vis.create_window()
     vis.add_geometry(pcd)
     vis.run()  # user changes the view and press "q" to terminate
@@ -104,9 +103,13 @@ def reconstruct(room_name, cam_poses):
     else:
         print("No result file, please check your folder again!")
         return
-    vis = o3d.Visualizer()
+    if VIS_ALL or VIS_FINAL:
+        vis = o3d.Visualizer()
+        vis.create_window()
+        print("created a window")
+
     for start_index_key in start_key_list:
-        print(start_index_key)
+        print("start index", start_index_key)
         result_pcd_folder = os.path.join(proj_path, "result", "pcd", start_index_key)
         if not os.path.exists(result_pcd_folder):
             os.makedirs(result_pcd_folder)
@@ -114,13 +117,14 @@ def reconstruct(room_name, cam_poses):
         viewpoint_file = os.path.join(result_pcd_folder, "view_point.json")
 
         for method in NBV_strategy:
-
+            print("method", method)
             result_pcd_file = os.path.join(result_pcd_folder, "{}_{}_{}.ply".format(dataset_name, room_name, method))
             result_pcd_folder_method = os.path.join(proj_path, "result", start_index_key,method)
 
             if (not os.path.exists(result_pcd_file)) or (VIS_ALL and (not os.path.exists(result_pcd_folder_method))):
-                vis.create_window()
-                print("created a window")
+                if VIS_ALL:
+                    trajectory = o3d.read_pinhole_camera_trajectory(viewpoint_file)
+                    print("created a window")
                 if VIS_ALL and (not os.path.exists(result_pcd_folder_method)):
                     os.makedirs(os.path.join(result_pcd_folder_method, "pcd"))
                     os.makedirs(os.path.join(result_pcd_folder_method, "depth"))
@@ -141,7 +145,7 @@ def reconstruct(room_name, cam_poses):
                     volume.integrate(rgbd, intrinsic, np.linalg.inv(current_pose))
                     pcd_scene = volume.extract_point_cloud()  # note the points are not isometric
                     if VIS_ALL:
-                        trajectory = o3d.read_pinhole_camera_trajectory(viewpoint_file)
+                        print("Visualise each frame")
                         result_pcd_file_temp = os.path.join(result_pcd_folder_method, "pcd", "{}_{}_{:03d}.png".format(dataset_name, room_name, count))
                         result_depth_file_temp = os.path.join(result_pcd_folder_method, "depth", "{}_{}_{:03d}.png".format(dataset_name, room_name, count))
                         result_rgb_file_temp = os.path.join(result_pcd_folder_method, "rgb","{}_{}_{:03d}.png".format(dataset_name, room_name, count))
@@ -149,13 +153,12 @@ def reconstruct(room_name, cam_poses):
                         copyfile(depth_path, result_depth_file_temp)
                         copyfile(rgb_path, result_rgb_file_temp)
 
-                        ctr = vis.get_view_control()
-                        mesh_frame = local_plt.o3d_coordinate(current_pose)
-                        #if count == 0:
-
                         vis.add_geometry(pcd_scene)
+                        mesh_frame = local_plt.o3d_coordinate(current_pose)
                         vis.add_geometry(mesh_frame)
 
+                        ctr = vis.get_view_control()
+                        #if count == 0:
                         ctr.convert_from_pinhole_camera_parameters(trajectory.intrinsic, trajectory.extrinsic[0])
                         vis.update_geometry()
                         vis.poll_events()
@@ -172,55 +175,63 @@ def reconstruct(room_name, cam_poses):
                         pcd_scene.points = o3d.Vector3dVector([])
                         pcd_scene.colors = o3d.Vector3dVector([])
 
-                    count = count +1
+                        vis.update_geometry()
+                        vis.poll_events()
+                        vis.update_renderer()
 
-                vis.destroy_window()
-                print("Destroyed a window and visualiser")
-                time.sleep(0.5)
+                    count = count + 1
+
                 if not os.path.exists(result_pcd_file):
-                    pcd_scene = volume.extract_point_cloud() # note the points are not isometric
+                    pcd_scene = volume.extract_point_cloud() #note the points are not isometric
                     o3d.write_point_cloud(result_pcd_file, pcd_scene)
-
             else:
                 if VIS_FINAL:
 
+                    print("created a visualiser")
                     pcd_scene = o3d.read_point_cloud(result_pcd_file)
                     # only trigger the view point set when there is no tarjectory file
                     if not os.path.exists(viewpoint_file):
                         print("Please manipulate the visualiser for a better view point, close to save!")
-                        save_view_point(pcd_scene, viewpoint_file)
+                        save_view_point(vis, pcd_scene, viewpoint_file)
                         return
 
-                    vis.create_window()
                     image_name = os.path.join(result_pcd_folder, "{}_pcd.png".format(method))
 
-                    ctr = vis.get_view_control()
-
                     trajectory = o3d.read_pinhole_camera_trajectory(viewpoint_file)
+                    print("read the view point")
 
                     vis.add_geometry(pcd_scene)
+                    print("Added the view point")
+
+                    ctr = vis.get_view_control()
+                    print("obtained view control")
                     ctr.convert_from_pinhole_camera_parameters(trajectory.intrinsic, trajectory.extrinsic[0])
+
+                    print("changed the view point")
 
                     vis.update_geometry()
                     vis.poll_events()
                     vis.update_renderer()
-
+                    print("updated the rendering")
                     vis.capture_screen_image(image_name)
+                    print("captured the screenshot")
                     # crop border
                     img = improc.crop_border(image_name, 0.08, 0.4)
                     #img = improc.imscale(img, 1.5)
                     improc.imwrite(image_name, img)
 
-                    time.sleep(0.5)
+                    time.sleep(2)
 
                     pcd_scene.points = o3d.Vector3dVector([])
                     pcd_scene.colors = o3d.Vector3dVector([])
 
                     vis.update_geometry()
                     vis.poll_events()
-
                     vis.update_renderer()
-                    vis.destroy_window()
+                    print("reset the point to empty")
+    if VIS_ALL or VIS_FINAL:
+        vis.destroy_window()
+        print("closed window and visualiser")
 
 
 def generate_reconstruction(room_name):
@@ -229,6 +240,7 @@ def generate_reconstruction(room_name):
     pose_file = os.path.join(proj_path, config["dataset_folder"], config["pose_file"])
     cam_poses, positions = io.read_camera_poses(pose_file)
     reconstruct(room_name, cam_poses)
+
 
 start_t = time.time()
 for name in selected_room_names:
